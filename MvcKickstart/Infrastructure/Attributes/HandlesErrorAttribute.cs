49e6a7bf32e185db22a73889d2905d83af69a7ae
Jim Geurts <jim@biacreations.com>
2012-10-07 16:23:35 -0500

    Log errors handled via the HandlesErrorAttribute.

--- MvcKickstart/Infrastructure/Attributes/HandlesErrorAttribute.cs
+++ MvcKickstart/Infrastructure/Attributes/HandlesErrorAttribute.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Mvc;
+using ServiceStack.Logging;
 using StructureMap;
 
 namespace MvcKickstart.Infrastructure.Attributes
@@ -6,16 +7,21 @@ namespace MvcKickstart.Infrastructure.Attributes
 	public class HandlesErrorAttribute : HandleErrorAttribute
 	{
 		private IMetricTracker Metrics { get; set; }
+		private ILog Log { get; set; }
 
 		public HandlesErrorAttribute()
 		{
 			Metrics = ObjectFactory.GetInstance<IMetricTracker>();
+			Log = LogManager.LogFactory.GetLogger(GetType());
 		}
 
 		public override void OnException(ExceptionContext filterContext)
 		{
 			Metrics.Increment(Metric.Error_Unhandled);
-
+			if (filterContext != null && filterContext.Exception != null)
+			{
+				Log.Error("Unhandled exception", filterContext.Exception);
+			}
 			base.OnException(filterContext);
 		}
 	}
