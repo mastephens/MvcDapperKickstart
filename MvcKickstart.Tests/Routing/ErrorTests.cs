c163e98ffaa2ac041dae5596c6a1a0ad54b4e284
Jim Geurts <jim@biacreations.com>
2012-10-07 16:53:46 -0500

    Fix issue with GetOrPostAttribute not registering routes properly. Add tests for ErrorController

---
+++ MvcKickstart.Tests/Routing/ErrorTests.cs
@@ -0,0 +1,25 @@
+﻿using MvcContrib.TestHelper;
+using MvcKickstart.Controllers;
+using NUnit.Framework;
+
+namespace MvcKickstart.Tests.Routing
+{
+	public class ErrorTests : RouteTestBase
+	{
+		[Test]
+		public void DefaultRoute()
+		{
+			"~/Error".ShouldMapTo<ErrorController>(x => x.Index());
+		}
+		[Test]
+		public void InvalidPageRoute()
+		{
+			"~/Invalid-Page".ShouldMapTo<ErrorController>(x => x.InvalidPage());
+		}
+		[Test]
+		public void NoPermissionRoute()
+		{
+			"~/No-Permission".ShouldMapTo<ErrorController>(x => x.NoPermission());
+		}
+	}
+}
--- MvcKickstart/Infrastructure/Attributes/GetOrPostAttribute.cs
+++ MvcKickstart/Infrastructure/Attributes/GetOrPostAttribute.cs
@@ -12,7 +12,7 @@ namespace MvcKickstart.Infrastructure.Attributes
 		/// Specify a route for an action constrained to requests providing an httpMethod value of GET or POST.
 		/// </summary>
 		/// <param name="routeUrl">The url that is associated with this action</param>
-		public GetOrPostAttribute(string routeUrl) : base(routeUrl, HttpVerbs.Get | HttpVerbs.Post | HttpVerbs.Head)
+		public GetOrPostAttribute(string routeUrl) : base(routeUrl, HttpVerbs.Get, HttpVerbs.Post, HttpVerbs.Head)
 		{
 		}
 	}
